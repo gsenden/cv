21bf52850a56381deb3b46195343d82136571294
Ger Senden <gsenden@outlook.com>
2021-03-13 09:09:16 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-mkdir -p dist
-npx resume export ./dist/resume.pdf --theme . > /dev/null 2>&1
-echo "Generated ./dist/resume.pdf"
-npx resume export ./dist/resume.html --theme . > /dev/null 2>&1
-echo "Generated ./dist/resume.html"
+for resumePath in "resumes"/*
+do
+  mkdir -p dist
+  resumeFile=$(basename "$resumePath")
+  resume="${resumeFile%.*}"
+  cp $resumePath resume.json
+  npx resume export ./dist/${resume}.pdf --theme . > /dev/null 2>&1
+  npx resume export ./dist/${resume}.html --theme . > /dev/null 2>&1
+  rm resume.json
+  echo '==> Resume' $resume 'generated in ./dist'
+done
